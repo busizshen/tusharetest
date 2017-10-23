c5411dc683d2154bd5fc877dff51f9ab6a6084a0
wanghj <78732833@qq.com>
2017-10-23 14:00:40 +0800

    测试1

--- tushareTest/oneTick.1.py
+++ tushareTest/oneTick.1.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 from sklearn import svm
-
+import tushare as ts
 import pandas as pd
 import numpy as np
 import datetime
-# import pandas.io.data as web
 from pandas_datareader import data as web
 import math
 import matplotlib.pyplot as plt
@@ -32,98 +31,112 @@ from sklearn.linear_model import LinearRegression
 # 002230
 # pp = ts.get_hist_data('600848')
 # pp.to_csv('600848.csv',encoding='utf-8')
-
-# pp = ts.get_hist_data('002230')
-# pp.to_csv('002230.csv',encoding='utf-8')
-
-df = pd.read_csv('002230.csv')
-# print(lines)
-# print(lines.shape[0])
-# lines.sort_index(axis=1, ascending=False) 
-
-# index=[]
-# for i in range(0,700):
-#     # print(i)
-#     index.append(i)
-# index.reverse()
-# print(index)
-# lines1=lines.reindex(index)
-ff=df['date']
-
-print(ff.size)
-
-df = df[['open',  'high',  'low',  'close', 'volume','date']]
-df['HL_PCT'] = (df['high'] - df['low']) / df['close'] * 100.0
-df['PCT_change'] = (df['close'] - df['open']) / df['open'] * 100.0
-df = df[['close', 'HL_PCT', 'PCT_change', 'volume','date']]
-# df['date'] = df['date'] #datetime.datetime.strptime(df['date'], "%Y-%m-%d")
-df['date'] =  pd.to_datetime(df['date'], format='%Y-%m-%d')
-df['date'] = pd.to_numeric(df['date'])
-
-#print(df.head())
-print(df[2:3]  )
-forecast_col = 'close'
-df.fillna(value=-99999, inplace=True)
-forecast_out = int(math.ceil(0.01 * len(df)))
-
-df['label'] = df[forecast_col].shift(-forecast_out)
-
-print(df.shape)
-print(df.tail())
-X = np.array(df.drop(['label'], 1))
+def getData(num):
+    otherStyleTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    pp = ts.get_hist_data(num)
+    pp.to_csv('%s-%s.csv'%(num,otherStyleTime),encoding='utf-8')
 
 
-X = preprocessing.scale(X)
+def trainData(fileName):
+    df = pd.read_csv(fileName,index_col='date')
+    # print(lines)
+    # print(lines.shape[0])
+    # lines.sort_index(axis=1, ascending=False)
 
-X_lately = X[-forecast_out:]
-X = X[:-forecast_out]
-df.dropna(inplace=True)
-print(X)
-print(X_lately)
-y = np.array(df['label'])
-#print(y)
-print(X.shape)
-print(y.shape)
+    # index=[]
+    # for i in range(0,700):
+    #     # print(i)
+    #     index.append(i)
+    # index.reverse()
+    # print(index)
+    # lines1=lines.reindex(index)
+    # ff=df['date']
+    # df=df.sort_index(ascending=False)
+    df=df.sort_index()
+    # df.reindex(df['date'])
 
-X_train, X_test, y_train ,y_test = cross_validation.train_test_split(X,y,test_size=0.2)
+    df = df[['open',  'high',  'low',  'close', 'volume']]
+    # print(df)
+    df['HL_PCT'] = (df['high'] - df['low']) / df['close'] * 100.0
+    df['PCT_change'] = (df['close'] - df['open']) / df['open'] * 100.0
+    df = df[['close', 'HL_PCT', 'PCT_change', 'volume']]
+    # df['date'] = df['date'] #datetime.datetime.strptime(df['date'], "%Y-%m-%d")
+    # df['date'] =  pd.to_datetime(df['date'], format='%Y-%m-%d')
+    # df['date'] = pd.to_numeric(df['date'])
 
-clf = LinearRegression()
-clf.fit(X_train,y_train)
-accuracy = clf.score(X_test,y_test)
+    #print(df.head())
+    # print(df[2:3])
+    forecast_col = 'close'
+    df.fillna(value=-99999, inplace=True)
+    forecast_out = int(math.ceil(0.01 * len(df)))
 
-print(accuracy)
-
-forecast_set = clf.predict(X_lately)
-
-print(forecast_set,accuracy,forecast_out)
-
-style.use('ggplot')
-
-df['Forecast']=np.nan
-
-# last_date = df.iloc[-1]
-# # print('-------',last_date)
-
-# last_unix = last_date.date
-# print(last_date,last_unix)
-# one_day = 86400
-# next_unix = last_unix + one_day
-# print("---------",next_unix)
-index=-1
-for i in forecast_set :
-    print(df.iloc[index].date)
-    next_date = datetime.datetime.fromtimestamp(df.iloc[index].date)
-    index=index-1
-    # next_unix += 86400
-    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+    df['label'] = df[forecast_col].shift(-forecast_out)
+    print(df['label'],"-----------------------")
+    print(df.shape)
+    print(df.tail())
+    X = np.array(df.drop(['label'], 1))
 
 
-print(df.tail())
+    X = preprocessing.scale(X)
 
-df['Close'].plot()
-df['Forecast'].plot()
-plt.show()
+    X_lately = X[-forecast_out:]
+    X = X[:-forecast_out]
+    df.dropna(inplace=True)
+    print(X)
+    print(X_lately)
+    y = np.array(df['label'])
+    #print(y)
+    print(X.shape)
+    print(y.shape)
 
+
+    X_train, X_test, y_train ,y_test = cross_validation.train_test_split(X,y,test_size=0.3)
+
+    clf = LinearRegression()
+    clf.fit(X_train,y_train)
+    accuracy = clf.score(X_test,y_test)
+
+    print(accuracy,"---------score------")
+
+    forecast_set = clf.predict(X_lately)
+
+    print(forecast_set,accuracy,forecast_out)
+
+    style.use('ggplot')
+
+    df['Forecast']=np.nan
+
+    last_date = df.iloc[-1]
+    # print('-------',last_date)
+
+    # last_unix = last_date.date
+    # print(last_date,last_unix)
+    # one_day = 86400
+    # next_unix = last_unix + one_day
+    index_num=0
+    index=datetime.datetime.now()
+    # curDate = context.current_dt.date()
+    # preDate = index + datetime.timedelta(days=index_num)
+    for i in forecast_set :
+        # print(df.iloc[index].date)
+        # next_date = datetime.datetime.fromtimestamp(df.iloc[index].date)
+
+        # next_unix += 86400
+        df.ix[index.strftime('%Y-%m-%d')] = [np.nan for _ in range(len(df.columns)-1)]+[i]
+        index_num+=1
+        # index=index.timedelta(days=index_num)
+        index = index + datetime.timedelta(days=index_num)
+        print(index)
+
+
+
+    print(df)
+
+    df['close'].plot()
+    df['Forecast'].plot()
+    plt.show()
+# getData('600887')
+trainData('600887-20171023134838.csv')
 # import datetime
 
 # (datetime.datetime.now() - datetime.timedelta(days = 100)).strftime("%Y-%m-%d")
