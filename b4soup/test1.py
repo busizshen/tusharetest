0bdc29f0a60bed8780daeb3c86339c5d4c055d6e
wanghj <78732833@qq.com>
2018-01-20 15:02:36 +0800

    191

--- b4soup/test1.py
+++ b4soup/test1.py
@@ -1,24 +1,25 @@
 import codecs
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
-baseUrl="https://www.qu.la/"
-html = urlopen("https://www.qu.la/book/16431/")
-# print(html.read().decode("utf-8"))
-html=html.read().decode("utf-8")
-soup=BeautifulSoup(html,"lxml")
-# print(soup.prettify())
-print(soup.title.string)
-# print(soup.head)
-print(soup)
-# print(soup.dd.a['href'])
-# print(soup.find_all('dd'))
-tt = soup.find_all('dd')
-for url in tt:
-    print(url.a['href'])
-    html = urlopen("https://www.qu.la/book"+url.a['href'])
-# with  codecs.open("filename.txt", 'a', "utf-8") as fileObj:
-#     fileObj.write("%s\n" % soup)
+def getBook(book):
+    baseUrl="https://www.qu.la/"
+    bookUrl="https://www.qu.la/book/%s/"%(book)
+    html = urlopen(bookUrl)
+    # print(html.read().decode("utf-8"))
+    html=html.read().decode("utf-8")
+    soup=BeautifulSoup(html,"lxml")
+    # print(soup.prettify())
+    print(soup.title.string)
+    # print(soup.head)
+    print(soup)
+    # print(soup.dd.a['href'])
+    # print(soup.find_all('dd'))
+    tt = soup.find_all('dd')
+    for url in tt:
+        print(url.a['href'])
+        # html = urlopen("https://www.qu.la/book"+url.a['href'])
+        with  codecs.open("filenameUrl%s.txt"%(book), 'a', "utf-8") as fileObj:
+            fileObj.write("%s,%s\n" % (url.a['href'],url.get_text()))
 # bsObj=BeautifulSoup(html,"lxml")
 # divdd=bsObj.find('div', id_='list')
 # # print("----------",divdd)
@@ -26,3 +27,4 @@ for url in tt:
 # ulList=divdd.find_all("dd")
 # for ul in ulList:
 #     print(ul.href)
+getBook("16431")
--- b4soup/test2.py
+++ b4soup/test2.py
@@ -2,18 +2,31 @@ import codecs
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-baseUrl="http://www.qu.la/"
-html = urlopen("https://www.qu.la/book/16431/6658470.html")
-# print(html.read().decode("utf-8"))
-html=html.read().decode("utf-8")
-soup=BeautifulSoup(html,"lxml")
-# print(soup.prettify())
-print(soup.title.string)
-# print(soup.head)
-print(soup.h1.get_text())
-content=soup.find( id='content')
-[x.extract() for x in content.find_all('script')]
-print(content.get_text())
 
-with  codecs.open("filename1.txt", 'a', "utf-8") as fileObj:
-    fileObj.write("%s\n" % soup)
+        # print("stdout:%s"%line)
+def gettext(url):
+    baseUrl="https://www.qu.la%s"%(url)
+    print(baseUrl)
+    html = urlopen(baseUrl)
+    # print(html.read().decode("utf-8"))
+    html=html.read().decode("utf-8")
+    soup=BeautifulSoup(html,"lxml")
+    # print(soup.prettify())
+    print(soup.title.string)
+    with codecs.open("filename1.txt", 'a', "utf-8") as fileObj:
+        fileObj.write("%s\n" % soup.title.string)
+    # print(soup.head)
+    print(soup.h1.get_text())
+    content=soup.find( id='content')
+    [x.extract() for x in content.find_all('script')]
+    print(content.get_text())
+    with codecs.open("filename1.txt", 'a', "utf-8") as fileObj:
+        fileObj.write("%s\n" % content.get_text())
+
+
+with codecs.open("filenameUrl16431.txt",encoding="utf-8") as file:
+    for line in file.readlines():
+        url=line.split(",")
+        if len(url)>1:
+            print(url[0])
+            gettext(url[0])
