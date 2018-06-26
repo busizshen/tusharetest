a05919a5b41288f97a0b73bb5cec97400daa959b
wangl <wanglei19930915@126.com>
2018-06-26 23:58:20 +0800

    词云制作 by lay 20180626

--- wordCloud/test1.py
+++ wordCloud/test1.py
@@ -1,31 +1,38 @@
 #coding=utf-8
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud,STOPWORDS
 import jieba.analyse
-from PIL import Image
-import numpy as np
-import chardet
-from matplotlib.font_manager import FontProperties
+from wordcloud import WordCloud, ImageColorGenerator
+from scipy.misc import imread
+import matplotlib.pyplot as plt
 
-# adding movie script specific stopwords
-stopwords = set(STOPWORDS)
-stopwords.add("int")
-stopwords.add("ext")
 
-font_set = FontProperties(fname=r"c:\windows\fonts\simsun.ttc", size=12)
+class wc:
+    def __init__(self, txt_file, img_file, font_file):
+        self.f = open(txt_file,encoding='utf-8')
 
-text_from_file_with_apath = open('best1.txt','r',encoding='utf-8').read()
+        self.txt = self.f.read()
+        self.f.close()
+        self.tags = jieba.analyse.extract_tags(self.txt, topK=100)
+        # topK说白了就是返回几个关键词
+        self.text = ' '.join(self.tags)  # 把分词链接起来，加空格因为英文靠空格分词
+        self.img = imread(img_file)
+        self.wc = WordCloud(font_path=font_file, background_color='white', max_words=100, mask=self.img, max_font_size=50)
+        ###直接在这里进行猜###
+        # font_path指的是字体文件路径，因为wordcloud自带的字体不支持中文所以我们要指定一个字体文件，否者输出的图片全是框框
+        # background_color 默认是黑色　我设置成白色
+        # max_words最大显示的词数
+        # mask 背景图片
+        # max_font_size　最大字体字号
+        self.word_cloud = self.wc.generate(self.text)
 
-abel_mask = np.array(Image.open("6.png"))
 
-wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all=True)
-wl_space_split = " ".join(wordlist_after_jieba)
-##解决乱码的关键在于  font_path='./fonts/simhei.ttf'
-my_wordcloud = WordCloud(background_color="white",width=1000,height=1000,max_words=800,max_font_size=80,min_font_size=20,font_path='./fonts/simhei.ttf',mask=abel_mask,stopwords=stopwords, margin=10,
-               random_state=1).generate(wl_space_split)
+    def show_wc(self):
+        # img_color = ImageColorGenerator(self.img)
+        plt.imshow(self.word_cloud)
+        # 可以通过 plt.imshow(self.wc.recolor(color_func=img_color))使图片颜色跟字体颜色一样
+        plt.axis("off")
+        plt.show()
 
-plt.imshow(my_wordcloud)
-plt.title("Custom colors")
-plt.title(u"周婷的自我介绍",fontproperties=font_set)
-plt.axis("off")
-plt.show()
+
+if __name__ == '__main__':
+    mywc = wc('test2.txt', 'webImg2.jpg', 'c:\windows\\fonts\simsun.ttc')
+    mywc.show_wc()
